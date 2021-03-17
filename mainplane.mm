874d59d006b9f23d8493a879d6b07769369c9e60
Anty <65603607+AntyElo@users.noreply.github.com>
2021-03-17 10:45:32 +0200

    Add files via upload

---
+++ mainplane.mm
@@ -0,0 +1,179 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="&#x410;&#x440;&#x445;&#x430;&#x438;&#x447;&#x43d;&#x44b;&#x439;&#xa;c++ &#x43a;&#x43e;&#x434;" FOLDED="false" ID="ID_900399244" CREATED="1612023302285" MODIFIED="1614266488602" STYLE="oval">
+<icon BUILTIN="checked"/>
+<font NAME="Liberation Sans" SIZE="12" BOLD="true"/>
+<hook NAME="MapStyle">
+    <conditional_styles>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.connection" LAST="false">
+            <node_periodic_level_condition PERIOD="2" REMAINDER="1"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.topic" LAST="false">
+            <node_level_condition VALUE="2" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.subtopic" LAST="false">
+            <node_level_condition VALUE="4" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="styles.subsubtopic" LAST="false">
+            <node_level_condition VALUE="6" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+    </conditional_styles>
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="Arial" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.connection" COLOR="#606060" STYLE="fork">
+<font NAME="Arial" SIZE="8" BOLD="false"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Xeon (0x, 1x; Old-Stable)" LOCALIZED_STYLE_REF="styles.topic" POSITION="right" ID="ID_1928271989" CREATED="1612024354241" MODIFIED="1615962191166" HGAP_QUANTITY="94.24999760836373 pt" VSHIFT_QUANTITY="83.99999749660499 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="1" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1278506500" STARTINCLINATION="-54;190;" ENDINCLINATION="17;89;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#1056;&#1072;&#1079;&#1074;&#1080;&#1083;&#1086;&#1089;&#1100; &#1074; Oxigen &#1080; Argon
+    </p>
+    <p>
+      &#1089; &#1074;&#1077;&#1088;&#1089;&#1080;&#1080; 12 (&#1087;&#1086;&#1089;&#1083;&#1077;&#1076;&#1085;&#1077;&#1081;)
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="[Special]" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_1272258128" CREATED="1612027751483" MODIFIED="1615961920189" HGAP_QUANTITY="33.49999941885474 pt" VSHIFT_QUANTITY="-53.249998413026375 pt">
+<icon BUILTIN="info"/>
+<node TEXT="Oxigen (In-FilesLibs; 2x)" LOCALIZED_STYLE_REF="defaultstyle.note" ID="ID_489340317" CREATED="1612024742787" MODIFIED="1615961915005" HGAP_QUANTITY="64.99999848008162 pt" VSHIFT_QUANTITY="5.249999843537811 pt">
+<icon BUILTIN="checked"/>
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</node>
+<node TEXT="OLD" LOCALIZED_STYLE_REF="defaultstyle.note" ID="ID_1673503109" CREATED="1612027757886" MODIFIED="1615961917629" HGAP_QUANTITY="64.24999850243334 pt" VSHIFT_QUANTITY="5.99999982118608 pt">
+<icon BUILTIN="checked"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#1057;&#1090;&#1072;&#1088;&#1072;&#1103; &#1079;&#1072;&#1087;&#1080;&#1089;&#1100; sff: 0x, 1x, 2x
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</node>
+<node TEXT="libsff" LOCALIZED_STYLE_REF="defaultstyle.note" ID="ID_1313184233" CREATED="1612027764754" MODIFIED="1615961920189" HGAP_QUANTITY="61.249998591840296 pt" VSHIFT_QUANTITY="0.7499999776482564 pt">
+<icon BUILTIN="checked"/>
+<arrowlink SHAPE="LINE" COLOR="#000000" WIDTH="1" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1826992508" STARTINCLINATION="-125;-110;" ENDINCLINATION="62;-115;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#1056;&#1077;&#1072;&#1083;&#1080;&#1079;&#1072;&#1094;&#1080;&#1103; sff &#1074; &#1074;&#1080;&#1076;&#1077; &#1073;&#1080;&#1073;&#1083;&#1077;&#1086;&#1090;&#1077;&#1082;&#1080;
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</node>
+<node TEXT="OLD2" LOCALIZED_STYLE_REF="defaultstyle.note" ID="ID_266121129" CREATED="1615961262893" MODIFIED="1615961926365" HGAP_QUANTITY="61.99999856948857 pt" VSHIFT_QUANTITY="-27.74999917298558 pt">
+<icon BUILTIN="checked"/>
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<node TEXT="Argon (Stable)" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_1826992508" CREATED="1612024801530" MODIFIED="1615961965312" HGAP_QUANTITY="37.249999307095976 pt" VSHIFT_QUANTITY="36.74999890476469 pt">
+<icon BUILTIN="checked"/>
+<arrowlink SHAPE="LINE" COLOR="#000000" WIDTH="1" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_54085366" STARTINCLINATION="141;104;" ENDINCLINATION="35;-17;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="LINE" COLOR="#000000" WIDTH="1" TRANSPARENCY="200" DASH="7 7" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1278506500" STARTINCLINATION="85;60;" ENDINCLINATION="34;-22;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="Neon (Testing)" LOCALIZED_STYLE_REF="defaultstyle.note" ID="ID_154888532" CREATED="1612024890510" MODIFIED="1615961962023" HGAP_QUANTITY="19.24999984353782 pt" VSHIFT_QUANTITY="-10.499999687075622 pt">
+<icon BUILTIN="checked"/>
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</node>
+<node TEXT="Neon-tmp (Unstable)" LOCALIZED_STYLE_REF="defaultstyle.note" ID="ID_1349803354" CREATED="1612025037326" MODIFIED="1615962042277" HGAP_QUANTITY="18.49999986588956 pt" VSHIFT_QUANTITY="7.499999776482594 pt">
+<icon BUILTIN="checked"/>
+<arrowlink SHAPE="LINE" COLOR="#000000" WIDTH="1" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_476398032" STARTINCLINATION="189;-20;" ENDINCLINATION="128;-7;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="[Current]" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_1225124827" CREATED="1615961429926" MODIFIED="1615962364774" HGAP_QUANTITY="42.49999915063383 pt" VSHIFT_QUANTITY="-30.74999908357861 pt">
+<icon BUILTIN="info"/>
+<node TEXT="Neon-tmp" LOCALIZED_STYLE_REF="defaultstyle.note" ID="ID_476398032" CREATED="1615961973247" MODIFIED="1615962110129" HGAP_QUANTITY="59.74999863654379 pt" VSHIFT_QUANTITY="-20.24999939650299 pt">
+<icon BUILTIN="unchecked"/>
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</node>
+<node TEXT="Neon(GUI)" LOCALIZED_STYLE_REF="defaultstyle.note" ID="ID_54085366" CREATED="1615961609516" MODIFIED="1615962100134" HGAP_QUANTITY="57.49999870359901 pt" VSHIFT_QUANTITY="2.9999999105930364 pt">
+<icon BUILTIN="unchecked"/>
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</node>
+<node TEXT="Xeon(noGUI+2Argon)" LOCALIZED_STYLE_REF="defaultstyle.note" ID="ID_1278506500" CREATED="1615961549765" MODIFIED="1615962094137" HGAP_QUANTITY="57.499998703599005 pt" VSHIFT_QUANTITY="19.499999418854735 pt">
+<icon BUILTIN="checked"/>
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</node>
+<node TEXT="Icons" LOCALIZED_STYLE_REF="defaultstyle.note" ID="ID_108114305" CREATED="1615962258277" MODIFIED="1615962364772" HGAP_QUANTITY="23.749999709427364 pt" VSHIFT_QUANTITY="13.49999959766866 pt">
+<icon BUILTIN="addition"/>
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</node>
+</node>
+</node>
+<node TEXT="&#x41a;&#x430;&#x440;&#x442;&#x430; Miniformulas (sff)" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1821988531" CREATED="1612026679253" MODIFIED="1615961198230" HGAP_QUANTITY="14.249999575316945 pt" VSHIFT_QUANTITY="145.4999956637622 pt">
+<icon BUILTIN="info"/>
+<font NAME="Liberation Sans" SIZE="12" BOLD="true"/>
+<hook NAME="FreeNode"/>
+<hook URI="Icons/appGRaw.gif" SIZE="6.5102916" NAME="ExternalObject"/>
+</node>
+</node>
+</map>
